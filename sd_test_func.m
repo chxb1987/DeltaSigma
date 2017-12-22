5b1085d490dc672127a0a324c748df971356e399
Nishant Mehrotra <nishantm1996@iitkgp.ac.in>
2017-12-22 16:58:36 +0530

    Update sd_test_func.m

--- sd_test_func.m
+++ sd_test_func.m
@@ -1,4 +1,4 @@
-function [snrv,v] = sd_test_func (N,fs,f,u,w,order,OSR,ntf,nlev) 
+function [snrv,v,y,yt,vt] = sd_test_func (N,fs,f,u,w,order,OSR,ntf,nlev) 
 ut = u+w;
 [vw,xnw,xmaxw,yw] = simulateDSM(w,ntf,nlev);
 [v,xn,xmax,y] = simulateDSM(u,ntf,nlev);
@@ -8,6 +8,6 @@ hwv = fftshift(abs(fft(v'.*hanning(N+1))))./(N+1); hwvw = fftshift(abs(fft(vw'.*
 hwut = fftshift(abs(fft(ut'.*hanning(N+1))))./(N+1); hwvt = fftshift(abs(fft(vt'.*hanning(N+1))))./(N+1);
 hwu = fftshift(abs(fft(u'.*hanning(N+1))))./(N+1); hwuw = fftshift(abs(fft(w'.*hanning(N+1))))./(N+1);
 snru = 10*log10(sum(hwu(fint).^2)/sum(hwuw(fint).^2)); snrv = 10*log10(sum(hwv(fint).^2)/sum(hwvw(fint).^2));
-% plot(log10(fx),20.*log10(hwut)); hold on; plot(log10(fx),20.*log10(hwvt)); grid on;
-% legend(sprintf('snru = %f',snru),sprintf('snrv = %f',snrv));
+plot(log10(fx),20.*log10(hwut)); hold on; plot(log10(fx),20.*log10(hwvt)); grid on;
+legend(sprintf('snru = %f',snru),sprintf('snrv = %f',snrv));
 end
